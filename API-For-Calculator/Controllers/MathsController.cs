436ed83bc4e2204b6e11ead35d821a8f7845c58f
johnthebeloved <johnoluwale1@gmail.com>
2022-10-14 23:10:56 -0400

    Created Calculator API with basic functionality for issue #5

---
+++ API-For-Calculator/Controllers/MathsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_For_Calculator.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MathsController : ControllerBase
+    {
+        [HttpGet]
+        public double Addition(double firstNum, double secondNum)
+        {
+            return firstNum + secondNum;
+        }
+        [HttpGet]
+        public double Subtraction(double firstNum, double secondNum)
+        {
+            return firstNum - secondNum;
+        }
+        [HttpGet]
+        public double Multiplication(double firstNum, double secondNum)
+        {
+            return firstNum * secondNum;
+        }
+        [HttpGet]
+        public double Division(double firstNum, double secondNum)
+        {
+            return firstNum + secondNum;
+        }
+    }
+}
