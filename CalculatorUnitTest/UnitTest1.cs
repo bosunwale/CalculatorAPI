4576e98fd2c6ab6e451b7762d3da6e5dad224aa1
johnthebeloved <johnoluwale1@gmail.com>
2022-10-14 23:12:37 -0400

    Update Calculate Unit Test

--- CalculatorUnitTest/UnitTest1.cs
+++ CalculatorUnitTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 using AdditionLogic;
+using DivisionLogic;
 using MultiplicationLogic;
 using SubtractionLogic;
 
@@ -9,6 +10,8 @@ namespace CalculatorUnitTest
     {
         //Test for adding positive numbers and returning double
         [TestMethod]
+        [DataRow(2, 5, 7)]
+        [DataRow(8.5, 24, 32.5)]
         [DynamicData(nameof(GetPositiveNumbersToAdd), DynamicDataSourceType.Method)]
         public void Adding_Two_Positive_Numbers_and_return_Doubles(double leftNum, double rightNum, double expectedResult)
         {
@@ -34,6 +37,8 @@ namespace CalculatorUnitTest
 
         //Test for subtracting positive numbers and returning double
         [TestMethod]
+        [DataRow(10, 2, 8)]
+        [DataRow(25.5, 5.5, 20.0)]
         [DynamicData(nameof(GetPositiveNumbersToSubtract), DynamicDataSourceType.Method)]
         public void Subtracting_Two_Positive_Numbers_and_return_Doubles(double leftNum, double rightNum, double expectedResult)
         {
@@ -62,8 +67,10 @@ namespace CalculatorUnitTest
             return list;
         }
 
-        //Test for subtracting positive numbers and returning double
+        //Test for multiplying positive numbers and returning a double
         [TestMethod]
+        [DataRow(2, 5, 10)]
+        [DataRow(3.2, 5.8, 18.56)]
         [DynamicData(nameof(GetPositiveNumbersToMultiply), DynamicDataSourceType.Method)]
         public void Multiplication_of_Two_Positive_Numbers_and_return_Doubles(double leftNum, double rightNum, double expectedResult)
         {
@@ -80,6 +87,38 @@ namespace CalculatorUnitTest
                 var leftNum = random.NextDouble() * random.NextInt64();
                 var rightNum = random.NextDouble() * random.NextInt64();
                 var expectedResult = leftNum * rightNum;
+                if (expectedResult > 0)
+                {
+                    list.Add(new Object[] { leftNum, rightNum, expectedResult });
+                }
+                else
+                {
+                    i--;
+                }
+            }
+            return list;
+        }
+
+        //Test for dividing positive numbers and returning a double
+        [TestMethod]
+        [DataRow(4, 2, 2)]
+        [DataRow(5.75, 2, 2.875)]
+        [DynamicData(nameof(GetPositiveNumbersToDivide), DynamicDataSourceType.Method)]
+        public void Divisionn_of_Two_Positive_Numbers_and_return_Doubles(double leftNum, double rightNum, double expectedResult)
+        {
+            double result = Division.Divide(leftNum, rightNum);
+            Assert.AreEqual(expectedResult, result);
+        }
+        //Dynamic Method to test positive numbers for Division
+        private static IEnumerable<Object[]> GetPositiveNumbersToDivide()
+        {
+            var list = new List<Object[]>();
+            for (int i = 0; i < 10; i++)
+            {
+                Random random = new Random();
+                var leftNum = random.NextDouble() * random.NextInt64();
+                var rightNum = random.NextDouble() * random.NextInt64();
+                var expectedResult = leftNum / rightNum;
                 if (expectedResult > 0)
                 {
                     list.Add(new Object[] { leftNum, rightNum, expectedResult });
